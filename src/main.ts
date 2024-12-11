5562f6009197cbb21f12941a18db45e7d516562a
zhangenming <282126346@qq.com>
2024-12-11 15:09:32 +0800

    .

--- src/main.ts
+++ src/main.ts
@@ -3,22 +3,6 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-// dbg
-;(() => {
-  Object.defineProperties(Object.prototype, {
-    xx: {
-      get() {
-        if (this instanceof String) {
-          console.log(String(this))
-        } else {
-          console.log(this)
-        }
-        return this
-      },
-    },
-  })
-})()
-
 createApp(App).mount('#app')
 
 if (!location.search.includes('home')) {

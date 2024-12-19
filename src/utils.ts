c5f7138650e2e3943883f735db167b683e66e92f
zhangenming <282126346@qq.com>
2024-12-19 11:21:34 +0800

    move

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-import { positions } from './data'
+import { 位置, 棋子, type t棋子 } from './data'
 
 export const raw = [
   ['车', '马', '象', '士', '帅', '士', '象', '马', '车'],
@@ -28,23 +28,33 @@ export function 距离j(a: coord, b: coord) {
   return Math.abs(a.j - b.j)
 }
 
+export function get左侧位置({ i, j }: coord) {
+  return 位置[i][j - 1]
+}
+export function get右侧位置({ i, j }: coord) {
+  return 位置[i][j + 1]
+}
+export function get上侧位置({ i, j }: coord) {
+  return 位置[i - 1][j]
+}
+export function get下侧位置({ i, j }: coord) {
+  return 位置[i + 1][j]
+}
+
 export function get左侧全部位置({ i, j }: coord) {
-  return positions[i].slice(0, j).reverse()
+  return 位置[i].slice(0, j).reverse()
 }
-
 export function get右侧全部位置({ i, j }: coord) {
-  return positions[i].slice(j + 1)
+  return 位置[i].slice(j + 1)
 }
-
 export function get上侧全部位置({ i, j }: coord) {
-  return positions
+  return 位置
     .slice(0, i)
     .map((line) => line[j])
     .reverse()
 }
-
 export function get下侧全部位置({ i, j }: coord) {
-  return positions.slice(i + 1).map((line) => line[j])
+  return 位置.slice(i + 1).map((line) => line[j])
 }
 
 // 保证每次取数据都是随机的(添加到时候不必在意 直接添加就好)保证悔完棋的时候随机
@@ -79,9 +89,13 @@ export function test(bool: any, text: any) {
   }
 }
 
+export function 位置2棋子({ i, j }: coord): t棋子 | undefined {
+  return 棋子.find((e) => e.i === i && e.j === j)
+}
+
 export function ij2item(s: string) {
-  const [i, j] = s.split('-')
-  return positions[Number(i)][Number(j)]
+  const [i, j] = s.split('-').map((e) => Number(e))
+  return 棋子.find((e) => e.i === i && e.j === j)
 }
 
 export function 取反(tb: 'top' | 'bot') {

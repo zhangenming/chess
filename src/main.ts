9491f674b7a7d49801cb80f2fdd1499f0a941a7e
zhangenming <282126346@qq.com>
2025-01-10 09:10:23 +0800

    .

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,15 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import { isBoss } from './lib/constant'
 
 createApp(App).mount('#app')
+
+if (isBoss) {
+  const style = document.createElement('style')
+  style.textContent = `
+    body {  opacity: 0.05;}
+    body:hover {  opacity: 0.2;}
+  `
+  document.head.appendChild(style)
+}
